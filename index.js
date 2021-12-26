a68c7ac483dceec002deea7a42f0b2b58546b227
Aaron Elias <aaronelias007@gmail.com>
2021-12-26 01:49:22 -0500

    commit tree cc62fb3a45e675138775c3a2cac2872bdc5b03b6

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+} 
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+} 
+
+function destructivelyRemoveLastCat() {
+    cats.pop(1);
+} 
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(1);
+} 
+
+const allCats = [...cats, "Broom"];
+
+function appendCat(name) {
+    return allCats;
+}
+
+const someCats = ["Arnold", ...cats];
+
+function prependCat(name) {
+    return someCats;
+}
+
+const copyOfCats = cats.slice(1);
+
+function removeFirstCat() {
+    return copyOfCats 
+}
+
+const copiesOfCats = cats.slice(0,-1);
+
+function removeLastCat() {
+    return copiesOfCats 
+}
